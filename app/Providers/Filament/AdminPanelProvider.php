61b745f5d8c4dbd4cea5b3ccda5474d2486d8ac2
rbnset <rbn.setiyawan@gmail.com>
2026-08-25 07:53:12 +0700

    feature: add regis

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->databaseNotifications()
             ->viteTheme('resources/css/filament/admin/theme.css')
+            ->registration()
             ->login()
             // 1. NAMA APLIKASI
             ->brandName('Klinik Bidan Delima')
